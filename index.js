32f8c700883d669fef16a849a5ca53b90684e268
Andrei Makrushinemail <andrei18makrushin@gmail.com>
2022-08-03 15:08:42 +0500

    обновил задание

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 
 function text(arg){
-  if (typeof arg !== 'string') {
-    console.log('передана не строка');
-    return;
-  }else{
+  if (typeof arg == 'string') {
     sli = arg.trim().slice(0,30);
     if (sli.length < arg.length) {
-       sli += ' ...'
-    }
+       sli += ' ...'  
   }
-  
+  }
   console.log(sli);
-  
 };
 text('     Js совсем не сложный для новичка');
