4a12d87d3565a74962b24ce4bc59b0ffac946543
Andrei Makrushinemail <andrei18makrushin@gmail.com>
2022-08-04 19:35:48 +0500

    сделал 2 усложненное задание

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 
 let num = ['2', '4'];
 let arr = [2345, 35213, 56789, 98756, 456321, 67886, 1234567890].filter(e => num.includes(e.toString()[0]))
-onsole.log(arr);
+console.log(arr);
 
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
+}
