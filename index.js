a6c63b8bdf777aea6588cdb91cee5d40636d8a04
Andrei Makrushinemail <andrei18makrushin@gmail.com>
2022-08-04 19:19:08 +0500

    сделал 2 усложненное задание

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 
-const isNumber = function (num) {
-  return !isNaN(parseFloat(num)) && isFinite(num)  
-}
-do {
-  screenPrice = +prompt('Сколько будет стоить данная работа?');
-} while (!isNumber(screenPrice));
-
-console.log(screenPrice);
+let num = ['2', '4'];
+let arr = [2345, 35213, 56789, 98756, 456321, 67886, 1234567890].filter(e => num.includes(e.toString()[0]))
+onsole.log(arr);
 
