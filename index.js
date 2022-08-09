cf763f1535dcb117159362c03f7f09f6a34746cf
Andrei Makrushinemail <andrei18makrushin@gmail.com>
2022-08-09 21:50:00 +0500

    сделал усложненное

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
+const days = [
+  'Воскресенье',
+  'Понедельник',
+  'Вторник',
+  'Среда',
+  'Четверг',
+  'Пятница',
+  'Суббота'
+]
+const mon = [
+  'Января', 'Февраля', 'Марта', 'Апреля', 'Майя', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Нояюря', 'Декабря'
+]
+function date(){
+  let date = new Date()
+  let day = date.getDay()  
+  let numberDay = date.getDate()
+  let month = date.getMonth()
+  let year = date.getFullYear()
+  let hour = date.getHours()
+  let minutes = date.getMinutes()
+  let second = date.getSeconds()
 
-let num = ['2', '4'];
-let arr = [2345, 35213, 56789, 98756, 456321, 67886, 1234567890].filter(e => num.includes(e.toString()[0]))
-console.log(arr);
-
-function isPrime(n) {
-  for (let i = 2; i * i <= n; 
-   i == 2 ? i++ : i += 2)
-   if (n % i == 0) return false;
-  return n > 1;
+function nul(){
+  if (hour < 10) {
+    hour = '0' + hour
+  }
+  if (minutes < 10) {
+    minutes = '0' + minutes
+  }
+  if (second < 10) {
+    second = '0' + second
+  }
+  
 }
-const res = [...Array(100)].reduce((a, _, i) => a.concat(isPrime(i) ? `Делители числа ${i}: 1 и ${i}` : []) , []).join('\n');
-console.log(res)
+ nul()
+ function numToStr(num, arrText) {
+  if (num % 10 === 1 && num % 100 !== 11) { 
+    return arrText[0];
+  } else if (num % 10 >= 2 && num % 10 <= 4 && (num % 100 < 10 || num % 100 >= 20)) {
+    return arrText[1];            
+  }
+  return arrText[2];
+}
+  document.querySelector('.time').innerHTML = numberDay + '.' + (month + 1) + '.' + year + '-' + hour +':'+ minutes +':'+ second
+  document.querySelector('.times').innerHTML = 'Сегодня ' +days[day] + ' ' +numberDay + ' ' + 
+  mon[month] + ' ' + year + ' Года' + ' ' + hour + numToStr(hour, [' час', ' часов', ' часа']) + ' ' + minutes + numToStr(minutes, [' минута', ' минуты', ' минут']) + ' ' + second +numToStr(second, [' секунда', ' секунды', ' секунд'])
+}
+setInterval(date, 1000);
+
+
